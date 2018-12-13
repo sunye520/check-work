e2d01af33c8fb6fc7350d1b069ccec41ee372ccb
e <750539050@qq.com>
2018-12-13 11:32:47 +0800

    123

---
+++ src/main/java/com/yogee/youge/interfaces/check/TestInterface.java
@@ -0,0 +1,24 @@
+//package com.yogee.youge.interfaces.check;
+//
+//
+//import com.yogee.youge.interfaces.util.HttpResultUtil;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.HashMap;
+//
+//@Controller
+//@RequestMapping("${apiPath}")
+//@Slf4j
+//public class TestInterface {
+//
+//    @RequestMapping(value = "getTest",method = RequestMethod.POST)
+//    @ResponseBody
+//    public String getTest(){
+//        log.debug("getTest");
+//        return HttpResultUtil.successJson(new HashMap());
+//    }
+//}
